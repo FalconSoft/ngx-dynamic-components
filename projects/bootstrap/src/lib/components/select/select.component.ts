d023c581aaa8b94866c5468c3230dd6a24d77cc6
ViktorKukurba <viktor.kukurba@gmail.com>
2020-07-15 14:28:07 +0300

    Updated ng-select output events

--- projects/bootstrap/src/lib/components/select/select.component.ts
+++ projects/bootstrap/src/lib/components/select/select.component.ts
@@ -6,9 +6,20 @@ import { packageName } from '../../constants';
 @Component({
   selector: 'dc-select',
   template: `
-  <ng-select [items]="options" (change)="onSelect()" [ngStyle]="selectStyles"
+  <ng-select [items]="options" [ngStyle]="selectStyles"
       [(ngModel)]="componentDataModel" [bindLabel]="properties.bindLabel"
-      [bindValue]="properties.bindValue || 'value'" [placeholder]="properties.placeholder"></ng-select>
+      [bindValue]="properties.bindValue || 'value'" [placeholder]="properties.placeholder"
+      (add)="emitEvent(properties.add, $event)"
+      (blur)="emitEvent(properties.blur, $event)"
+      (change)="onSelect()"
+      (close)="emitEvent(properties.close, $event)"
+      (clear)="emitEvent(properties.clear, $event)"
+      (focus)="emitEvent(properties.focus, $event)"
+      (search)="emitEvent(properties.search, $event)"
+      (open)="emitEvent(properties.open, $event)"
+      (remove)="emitEvent(properties.remove, $event)"
+      (scroll)="emitEvent(properties.scroll, $event)"
+      (scrollToEnd)="emitEvent(properties.scrollToEnd, $event)"></ng-select>
     `,
   styles: [`
     :host ::ng-deep .ng-select.ng-select-single .ng-select-container {
@@ -21,7 +32,13 @@ import { packageName } from '../../constants';
 export class SelectComponent extends BaseUIComponent<SelectProperties> {
   onSelect(): void {
     this.changedDataModel.emit(this.dataModel);
+    // onSelect - deprecated.
     this.emitEvent(this.properties.onSelect);
+    this.emitEvent(this.properties.change);
+  }
+
+  emitEvent(prop: string, param?: any): void {
+    super.emitEvent(prop, param);
   }
 
   get selectStyles(): AttributesMap {
@@ -58,10 +75,7 @@ export class SelectProperties extends BindingProperties {
   })
   selectHeight?: string;
 
-  @propDescription({
-    description: 'On Select handler name.',
-    example: 'onCountrySelect',
-  })
+  /** @deprecated */
   onSelect?: string;
 
   @propDescription({
@@ -81,6 +95,78 @@ export class SelectProperties extends BindingProperties {
     example: 'id',
   })
   bindValue?: string;
+
+  @propDescription({
+    description: 'Allows to select multiple items.',
+    example: 'true'
+  })
+  multiple: boolean;
+
+  @propDescription({
+    description: 'Output add event handler.',
+    example: 'onAdd()',
+  })
+  add?: string;
+
+  @propDescription({
+    description: 'Output blur event handler.',
+    example: 'onBlur()',
+  })
+  blur?: string;
+
+  @propDescription({
+    description: 'Output change event handler.',
+    example: 'onChange()',
+  })
+  change?: string;
+
+  @propDescription({
+    description: 'Output close event handler.',
+    example: 'onClose()',
+  })
+  close?: string;
+
+  @propDescription({
+    description: 'Output clear event handler.',
+    example: 'onClear()',
+  })
+  clear?: string;
+
+  @propDescription({
+    description: 'Output focus event handler.',
+    example: 'onFocus()',
+  })
+  focus?: string;
+
+  @propDescription({
+    description: 'Output search event handler.',
+    example: 'onSearch()',
+  })
+  search?: string;
+
+  @propDescription({
+    description: 'Output open event handler.',
+    example: 'onOpen()',
+  })
+  open?: string;
+
+  @propDescription({
+    description: 'Output remove event handler.',
+    example: 'onRemove()',
+  })
+  remove?: string;
+
+  @propDescription({
+    description: 'Output scroll event handler.',
+    example: 'onScroll()',
+  })
+  scroll?: string;
+
+  @propDescription({
+    description: 'Output scrollToEnd event handler.',
+    example: 'onScrollToEnd()',
+  })
+  scrollToEnd?: string;
 }
 
 type SelectComponentConstrutor = new () => SelectComponent;
@@ -134,6 +220,8 @@ export const selectDescriptor: ComponentDescriptor<SelectComponentConstrutor, Se
       });
       xmlRes.childNodes = null;
     }
+
+    itemProperties.multiple = xmlRes.attrs.multiple === 'true';
 
     return {
       type: 'ng-select',
