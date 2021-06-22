53ba8582f42b33b099b4de4e7c1b20849535c663
Pavlo <Pavlo@YOGALAPTOP>
2021-06-22 15:09:57 +0100

    added query string to a component

--- projects/core/src/lib/html-elements/a/a.component.ts
+++ projects/core/src/lib/html-elements/a/a.component.ts
@@ -4,6 +4,7 @@ import { StyleProperties, propDescription, PropTypes } from '../../properties';
 import { ComponentExample, UIModel, ComponentDescriptor, Categories, XMLResult } from '../../models';
 import { BaseHTMLElement, parseHTMLUIModel } from '../../components/base-html-element';
 import { queryValue } from '../../utils';
+import { getTokenLoc } from 'jspython-interpreter/dist/common';
 
 @Directive()
 export class AComponent extends BaseHTMLElement<LinkProperties> { // eslint-disable-line
@@ -28,10 +29,46 @@ export class AComponent extends BaseHTMLElement<LinkProperties> { // eslint-disa
 
       if (this.properties.routerLink) {
         const router = this.injector.get(Router);
-        router.navigate([this.getPath()]);
+
+        const navigationExtras = {};
+        const queryParams = this.getQueryParams();
+
+        if (queryParams) {
+          (navigationExtras as any).queryParams = queryParams;
+        }
+        router.navigate([this.getPath()], navigationExtras);
         evt.preventDefault();
       }
     };
+  }
+
+  private getQueryParams(): any {
+    let queryParams = this.properties.queryString;
+    if (!queryParams) {
+      return null;
+    }
+
+    const matches = queryParams.match(/{\$\.[\w/]+}/g);
+    if (matches) {
+      matches.forEach(m => {
+        const path = m.replace(/[{}]+/, '');
+        const res = queryValue(this.dataModel, path);
+        queryParams = queryParams.replace(m, res);
+      });
+    }
+
+    function parseQuery(queryString) {
+      const query = {};
+      const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+      for (const pairText of pairs) {
+        const pair = pairText.split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+      }
+      return query;
+    }
+
+
+    return parseQuery(queryParams);
   }
 
   private getPath(): string {
@@ -80,6 +117,13 @@ export class LinkProperties extends StyleProperties {
     example: 'path/page',
   })
   routerLink?: string;
+
+  @propDescription({
+    description: 'Query String',
+    example: 'id=12&another={$.dataModelProperty}',
+  })
+  queryString?: string;
+
 }
 
 export const example: ComponentExample<UIModel<LinkProperties>> = {
@@ -111,6 +155,9 @@ export const aDescriptor: ComponentDescriptor<AComponentConstrutor, LinkProperti
     const uiModel = parseHTMLUIModel(xmlRes);
     if (xmlRes.attrs.routerLink) {
       uiModel.itemProperties.routerLink = xmlRes.attrs.routerLink;
+    }
+    if (xmlRes.attrs.queryParams) {
+      uiModel.itemProperties.queryParams = xmlRes.attrs.queryParams;
     }
     return uiModel;
   },
